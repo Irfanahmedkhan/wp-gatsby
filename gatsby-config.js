8ee715dff037f267a6e46cbc5e75c39bffdafebb
hamza <hamza.com>
2021-02-26 19:13:28 +0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: "wp-gatsby",
+  },
+  plugins: [
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
